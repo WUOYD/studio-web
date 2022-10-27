74ccbeedc286f58c7e085ed5c96843d53637d5e6
Nakatshima <fabian.zubler@gmx.ch>
2022-10-27 15:14:36 +0200

    Css Titel screen

--- src/App.vue
+++ src/App.vue
@@ -57,7 +57,7 @@
 
     @media (min-width: 1024px) {
         header {
-            display: flex;
+           
             place-items: center;
             padding-right: calc(var(--section-gap) / 2);
         }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,15 @@
 <template>
-    <header>
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="60" height="60" />
-        <div class="wrapper">
-            <p>Hello There ich bin de Header</p>
-            <nav>
-            </nav>
-        </div>
+    <header class="bg-black h-screen text-white grid grid-cols-12 grid-rows-3">
+       <div class="col-start-1 row-start-1 bigFont bg-blue-600">WHAT</div>
+       <div class="col-start-3 row-start-2 bigFont ">THE</div>
+       <div class="col-start-1 row-start-3 bigFont text-right">WEB</div>
     </header>
 </template>
-<style>
-
+<style scoped>
+.bigFont{
+    font-size: 10vh;
+    line-height: 90%;
+}
 </style>
+
+
