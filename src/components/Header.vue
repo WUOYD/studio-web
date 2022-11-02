c731d3e993e39236b0c3bf0dc51e7eb97191ebb7
Nakatshima <fabian.zubler@gmx.ch>
2022-11-02 15:17:02 +0100

    titel screen layout und animation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,34 +2,54 @@
     @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 
     header {
+        height: 250vh;
+     
+    }
+
+    header .test:last-of-type{
+        -webkit-text-stroke-width: 1px;
+        -webkit-text-stroke-color: white;
+        -webkit-text-fill-color: transparent;
+    }
+
+    .test{
+        margin-top: 2vw;
+        font-weight: 600;
         color: white;
-        font-size: 270px;
-        height: 100vh;
-        line-height:1;
+        font-size: 18vw;
+        line-height:0.8;
         z-index:  1;
+        font-family: 'Roboto', sans-serif;
     }
 
     header .test2 {
         width: 100%;
     }
 
-    .test2 div:nth-child(odd){
+    .test2 div:nth-of-type(1){
         transform: translateX(-100%);
     }
-    .test2 div:nth-child(even){
-        transform: translateX(100%);
+    .test2 div:nth-of-type(2){
+        transform: translateX(-150%);
     }
+    .test2 div:nth-of-type(3){
+        transform: translateX(200%);
+    }
+ 
+    
 
 
 </style>
 
 
+
+
 <template>
     <header>
         <div class="test2">
-            <div class="test">WHAT</div>
-            <div class="test">THE</div>
-            <div class="test">WEB</div>
+            <div class="test sp">WHAT</div>
+            <div class="test sp">THE</div>
+            <div class="test web">WEB</div>
         </div>
    </header>
 </template>
@@ -52,18 +72,22 @@ export default {
                     pin: true,
                     scrub: true,
                     start: () => "top 0",
-                    end: () => '+=100%',
+                    end: () => '+=150%',
                     markers: true,
                 }
             });
             tl.addLabel('initial');
-            tl.to(image.querySelectorAll('header div.test2 div.test'), {
+            tl.to(image.querySelectorAll('header div.test2 div.test:not(.sp)'), {
+                ease: 'none',
+                x: 500,
+                stagger: 0.0
+            }, 'initial');
+            tl.to(image.querySelectorAll('header div.test2 div.test.sp'), {
                 ease: 'none',
                 x: 0,
-                stagger: 0.2
-            });
+                stagger: 0.0
+            }, 'initial');
         });
-
         /*let sliders = document.querySelectorAll('.slider');
         sliders.forEach((slider, i) => {
             x = -slider.querySelector('.slider__items').offsetWidth + window.innerWidth;
@@ -79,9 +103,16 @@ export default {
                 }
             });
         });*/
-
         //ScrollTrigger.sort();
     }
+
+
+
+
+
+
+
+
 }
 
 
